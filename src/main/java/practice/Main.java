c15964280d7fb38c0d373a878db0376742ce14bf
k0na <pwpvpdru@yandex.ru>
2023-06-18 12:40:42 +0300

    lesson_11.9 homework codestyle fixes - rebase few methods in Main.java

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -22,30 +22,15 @@ public class Main {
                 break;
             }
 
-            if (!phoneBook.isName(input) && !phoneBook.isPhoneNumber(input) && !input.equals("LIST")) {
-                System.out.println("Неверный формат ввода");
-            }
-
-            if (input.equals("LIST") && !phoneBook.getAllContacts().isEmpty()) {
-                phoneBook.getAllContacts().forEach(System.out::println);
-            } else if (input.equals("LIST") && phoneBook.getAllContacts().isEmpty()) {
-                System.out.println("Телефонная книга пуста, сначала добавьте в нее данные для вывода");
-            }
+            checkInputToFormat(phoneBook, input);
+            checkInputToPrintCommand(phoneBook,input);
 
             if (phoneBook.isName(input)) {
                 name = input;
-                if (!phoneBook.getNames().contains(name) && phone == null) {
-                    System.out.println("Такого имени в телефонной книге нет.\n"
-                            .concat("Введите номер телефона для абонента ")
-                            .concat(String.format("\"%s\"", name)));
-                }
+                checkNameInContacts(phoneBook,phone,name);
             } else if (phoneBook.isPhoneNumber(input)) {
                 phone = input;
-                if (!phoneBook.getPhones().contains(phone) && name == null) {
-                    System.out.println("Такого номера в телефонной книге нет.\n"
-                            .concat("Введите имя абонента для номера ")
-                            .concat(String.format("\"%s\"", phone)));
-                }
+                checkPhoneInContacts(phoneBook,phone,name);
             }
 
             if (name != null && phone != null) {
@@ -54,6 +39,36 @@ public class Main {
                 phone = null;
                 name = null;
             }
+        }
+    }
+
+    public static void checkPhoneInContacts (PhoneBook phoneBook, String phone,String name){
+        if (!phoneBook.getPhones().contains(phone) && name == null) {
+            System.out.println("Такого номера в телефонной книге нет.\n"
+                    .concat("Введите имя абонента для номера ")
+                    .concat(String.format("\"%s\"", phone)));
+        }
+    }
+
+    public static void checkNameInContacts (PhoneBook phoneBook, String phone, String name){
+        if (!phoneBook.getNames().contains(name) && phone == null) {
+            System.out.println("Такого имени в телефонной книге нет.\n"
+                    .concat("Введите номер телефона для абонента ")
+                    .concat(String.format("\"%s\"", name)));
+        }
+    }
+
+    public static void checkInputToPrintCommand (PhoneBook phoneBook, String input){
+        if (input.equals("LIST") && !phoneBook.getAllContacts().isEmpty()) {
+            phoneBook.getAllContacts().forEach(System.out::println);
+        } else if (input.equals("LIST") && phoneBook.getAllContacts().isEmpty()) {
+            System.out.println("Телефонная книга пуста, сначала добавьте в нее данные для вывода");
+        }
+    }
+
+    public static void checkInputToFormat (PhoneBook phoneBook, String input) {
+        if (!phoneBook.isName(input) && !phoneBook.isPhoneNumber(input) && !input.equals("LIST")) {
+            System.out.println("Неверный формат ввода");
         }
     }
 }
